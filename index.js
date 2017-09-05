1fc9ddf5570a1996cf18f72e7b1b9ff295fa524b
gabidavila <gabidavila@gmail.com>
2017-09-05 19:58:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const app = "I don't do much."
+let dodger = document.getElementById("dodger");
+
+let moveDodger = function (pixels) {
+  let positionNumbers = dodger.style.left.replace("px", "");
+  let position = parseInt(positionNumbers, 10);
+
+  dodger.style.left = `${position + pixels}px`;
+};
+
+document.addEventListener("keydown", function (event) {
+  let position = parseInt(dodger.style.left.replace("px", ""));
+  let availableArea = dodger.parentNode.offsetWidth - dodger.offsetWidth;
+  if (event.which === 37 && position > 0) {
+    moveDodger(-1);
+  } else if (event.which === 39 && position < availableArea) {
+    moveDodger(1);
+  }
+});
